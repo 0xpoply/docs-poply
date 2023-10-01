effb85d2fabedda46476adebe2412089c5d4b840
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-10-02 00:50:45 +0330

    initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>POPLY</span>,
+  project: {
+    link: 'https://github.com/0xpoply',
+  },
+  chat: {
+    link: 'https://discord.gg/68wspMUhT7',
+  },
+  docsRepositoryBase: 'https://github.com/poply/docs-poply',
+  footer: {
+    text: 'Poply Docs',
+  },
+}
+
+export default config
