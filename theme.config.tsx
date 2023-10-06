ef81bbe0cd1d4120bb48207711c7fdcdf32280d8
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-10-06 14:52:42 +0330

    [FIX] content improved

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>POPLY</span>,
   project: {
-    link: 'https://github.com/0xpoply',
+    link: "https://github.com/0xpoply",
   },
   chat: {
-    link: 'https://discord.gg/68wspMUhT7',
+    link: "https://discord.gg/68wspMUhT7",
   },
-  docsRepositoryBase: 'https://github.com/poply/docs-poply',
+  docsRepositoryBase: "https://github.com/0xpoply/docs-poply",
   footer: {
-    text: 'Poply Docs',
+    text: "Poply Docs",
   },
-}
+};
 
-export default config
+export default config;
